197cb38ba208376231d1c3bf0dbbf8d936fc081a
Kilian von Pflugk <github@jumoog.io>
2024-12-02 22:08:01 +0100

    remove tiktok

--- index.ts
+++ index.ts
@@ -2,7 +2,6 @@ import signale from "signale";
 import { Instagram } from "./Instagram.ts";
 import { DiscordBot, rooms } from "./discord.js";
 import { Twitch } from "./twitch.ts";
-//import { TikTok } from "./tiktok.ts";
 
 // catch all possible errors and don't crash
 process.on('unhandledRejection', (reason: Error | any, p: Promise<any>) => {
@@ -15,17 +14,11 @@ process.on('unhandledRejection', (reason: Error | any, p: Promise<any>) => {
 const discord = new DiscordBot();
 const twitch = new Twitch();
 const instagram  = new Instagram();
-//const tiktok = new TikTok();
-
 
 instagram.on('post', async (message) => {
     await discord.sendIgPost(message);
 });
-/*
-tiktok.on('sendMessage', (message: string, room: rooms) => {
-    discord.sendMessage(message, room);
-});
-*/
+
 twitch.on('sendMessage', (message: string, room: rooms) => {
     discord.sendMessage(message, room);
 });
@@ -45,5 +38,3 @@ twitch.on('deleteCoolDown', () => {
 await discord.main();
 await twitch.main();
 await instagram.main();
-//await tiktok.checkLive();
-
--- tiktok.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="node" />
-import EventEmitter from 'events';
-export declare class TikTok extends EventEmitter {
-    private _isLive;
-    constructor();
-    checkLive(): Promise<void>;
-    private sendMessage;
-    private sendDebugMessage;
-}
