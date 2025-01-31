5f5e0581116ca83cb57477ec36c0dd5949f58b98
Bradyn <bradyntremmel17@gmail.com>
2025-01-31 11:13:27 +1100

    Change setActivity text

--- discord.ts
+++ discord.ts
@@ -193,7 +193,7 @@ export class DiscordBot extends EventEmitter {
 
     async onlineHandler(message: string) {
         if (this._discordClient.isReady()) {
-            this._discordClient.user?.setActivity({ name: "twitch.tv/annabelstopit", type: ActivityType.Watching })
+            this._discordClient.user?.setActivity({ name: "ΛNNΛBEL", type: ActivityType.Watching })
             this.sendMessage(message, Rooms.DEBUG)
         }
     }
